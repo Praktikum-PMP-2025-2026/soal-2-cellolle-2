89655ab0c6d197910149ca0c8fd7776c0cb33da3
cellolle <marcellopandiangan@gmail.com>
2026-05-11 08:56:48 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,19 +1,28 @@
+	/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+ *   Modul               : : 05 - Foundation of Algorithm
+ *   Hari dan Tanggal    : Senin, 11 May 2026
+ *   Nama (NIM)          : Marcello Menata Pandiangan (13224069)
+ *   Nama File           : prak5_13224069.c
+ *   Deskripsi           : Jadwal Misi Bertabrakan
+ *  SUMBER https://www.w3resource.com/c-programming-exercises/graph/c-graph-exercises-6.php
+ * 
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_n 100
+#define total 100
 
-// Function to add an edge to the graph
-void addEdge(int graph[MAX_n][MAX_n], int start, int end) {
+void addEdge(int graph[total][total], int start, int end) {
     graph[start][end] = 1;
-    graph[end][start] = 1; // For undirected graph
+    graph[end][start] = 1; 
 }
 
-// Recursive function to perform DFS and check for cycles
-int isCyclicUtil(int graph[MAX_n][MAX_n], int currentVertex, int parent, int visited[MAX_n]) {
+int isCyclicUtil(int graph[total][total], int currentVertex, int parent, int visited[total]) {
     visited[currentVertex] = 1;
 
-    for (int i = 0; i < MAX_n; i++) {
+    for (int i = 0; i < total; i++) {
         if (graph[currentVertex][i]) {
             if (!visited[i]) {
                 if (isCyclicUtil(graph, i, currentVertex, visited))
@@ -27,9 +36,8 @@ int isCyclicUtil(int graph[MAX_n][MAX_n], int currentVertex, int parent, int vis
     return 0;
 }
 
-// Function to check whether a graph contains a cycle
-int isCyclic(int graph[MAX_n][MAX_n], int n) {
-    int visited[MAX_n] = {0};
+int isCyclic(int graph[total][total], int n) {
+    int visited[total] = {0};
 
     for (int i = 0; i < n; i++) {
         if (!visited[i]) {
@@ -44,33 +52,12 @@ int isCyclic(int graph[MAX_n][MAX_n], int n) {
 int main() {
     int n, m;
     scanf("%d", &n);
-
-    if (n <= 0 || n > MAX_n) {
-        printf("Invalid number of n. Exiting...\n");
-        return 1;
-    }
-
-    int graph[MAX_n][MAX_n] = {0}; // Initialize the adjacency matrix with zeros
-
-    // Input the number of m
+    int graph[total][total] = {0}; 
+    
     scanf("%d", &m);
-
-    if (m < 0 || m > n * (n - 1) / 2) {
-        printf("Invalid number of m. Exiting...\n");
-        return 1;
-    }
-
-    // Input m and construct the adjacency matrix
     for (int i = 0; i < m; i++) {
         int start, end;
         scanf("%d %d", &start, &end);
-
-        // Validate input n
-        if (start < 0 || start >= n || end < 0 || end >= n) {
-            printf("Invalid n. Try again.\n");
-            i--;
-            continue;
-        }
 
         addEdge(graph, start, end);
     }
